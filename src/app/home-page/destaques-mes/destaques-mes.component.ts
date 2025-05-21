f65779c3cd280d4d8d90882b576b880adc4e0698
Douglas Batista <batista.douglasfs@gmail.com>
2025-05-21 15:03:21 +0000

    feat: componente com os destaques do mês

---
+++ src/app/home-page/destaques-mes/destaques-mes.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // para *ngFor, *ngIf
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'ds-destaques-mes',
+  templateUrl: './destaques-mes.component.html',
+  styleUrls: ['./destaques-mes.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule]
+})
+export class DestaquesMesComponent {
+  destaquesMes = [
+    {
+      id: '123456',
+      title: 'Flecha - Jullyana Cariri',
+      personagem: 'Jullyana Cariri',
+      autor: 'Autor 01',
+      thumbnail: 'https://museudapessoa.org/wp-content/uploads/avatar67f68ca59fe2c.png'
+    },
+    {
+      id: '789012',
+      title: 'Uma forma poética de resistência',
+      personagem: 'Patrícia Roberta Alves',
+      autor: 'Autor 02',
+      thumbnail: 'https://museudapessoa.org/wp-content/uploads/avatar/20250411192204Patr%C3%ADcia%20Roberta%20(2)%20(1).jpeg'
+    },
+    {
+      id: '789013',
+      title: 'História de vida, Maria Crescência, 98 anos',
+      personagem: 'Maria Crescência da Silva',
+      autor: 'Autor 03',
+      thumbnail: 'https://img.youtube.com/vi/GI9EWJO-CU8/hqdefault.jpg'
+    },
+    {
+      id: '789014',
+      title: 'Takoramono',
+      personagem: 'Nadir Shinhe',
+      autor: 'Nadir Shinhe',
+      thumbnail: 'https://museudapessoa.org/wp-content/uploads/avatar/20250425175453Screenshot_20250419_221345_Chrome.jpg'
+    }
+  ];
+}
