1cac4ef1b4a8a9f3d7f16bcfaedab0f0023e0a7e
Douglas Batista <batista.douglasfs@gmail.com>
2025-06-17 14:20:24 +0000

    fix: ajustes visuais no componente de destaques

--- src/app/home-page/destaques-mes/destaques-mes.component.ts
+++ src/app/home-page/destaques-mes/destaques-mes.component.ts
@@ -14,29 +14,25 @@ export class DestaquesMesComponent {
     {
       id: '123456',
       title: 'Flecha - Jullyana Cariri',
-      personagem: 'Jullyana Cariri',
-      autor: 'Autor 01',
+      resumo: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
       thumbnail: 'https://museudapessoa.org/wp-content/uploads/avatar67f68ca59fe2c.png'
     },
     {
       id: '789012',
       title: 'Uma forma poética de resistência',
-      personagem: 'Patrícia Roberta Alves',
-      autor: 'Autor 02',
+      resumo: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
       thumbnail: 'https://museudapessoa.org/wp-content/uploads/avatar/20250411192204Patr%C3%ADcia%20Roberta%20(2)%20(1).jpeg'
     },
     {
       id: '789013',
       title: 'História de vida, Maria Crescência, 98 anos',
-      personagem: 'Maria Crescência da Silva',
-      autor: 'Autor 03',
+      resumo: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
       thumbnail: 'https://img.youtube.com/vi/GI9EWJO-CU8/hqdefault.jpg'
     },
     {
       id: '789014',
       title: 'Takoramono',
-      personagem: 'Nadir Shinhe',
-      autor: 'Nadir Shinhe',
+      resumo: 'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão.',
       thumbnail: 'https://museudapessoa.org/wp-content/uploads/avatar/20250425175453Screenshot_20250419_221345_Chrome.jpg'
     }
   ];
