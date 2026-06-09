29395b4ee4eda9c92f44c0afb541b50d5d69ac99
Douglas Batista <batista.douglasfs@gmail.com>
2026-06-09 12:35:37 +0000

    feat: ajustes visuais na tela home e redes

---
+++ src/themes/institutopaulofreire/app/redes-ipf/redes-ipf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-redes-ipf-page',
+  templateUrl: './redes-ipf.component.html',
+  styleUrls: ['./redes-ipf.component.scss'],
+  standalone: true,
+})
+export class RedesIpfComponent {
+}
